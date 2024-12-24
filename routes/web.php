59d0d2531e3ffd17af32e107309a8ec454021ab0
Fadli1009 <argyfa3@gmail.com>
2024-12-24 09:25:27 +0700

    tambahin route bang fajar

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('punya-fadli.index');
 });
-Route::get('/fajar', function () {
-    return view('punya-fajar.tes');
+Route::get('/bangfajar', function () {
+    return view(view: 'punya-fajar.tes');
 });
